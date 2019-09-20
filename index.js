a432f62aa89fb6a831e49351c6c917cac8064000
Neo Kai Neng <neo.kai.neng@gmail.com>
2019-09-20 11:47:01 +0800

    create interactive would you rather based on input list

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Add event listeners
+
+
 function searchFunction() {
 	// Declare starting variables 
 	var input = document.getElementById("dbInput");
@@ -23,10 +26,38 @@ function searchFunction() {
 	}
 }
 
-function getResult() {
+function getResult1() {
     var text = document.getElementById("nameInput").value.trim();
+	var nameList = text.split("\n");
+	var result1 = document.getElementById("result1");
+	result1.innerHTML = "";
 	if(text.length > 0)
-		alert(text);
+	{
+		var name1 = nameList[Math.floor(Math.random() * nameList.length)];
+		var name2 = nameList[Math.floor(Math.random() * nameList.length)];
+		while(name2 == name1)
+			name2 = nameList[Math.floor(Math.random() * nameList.length)];
+		result1.innerHTML = "Would you rather go on a date with " + name1 + " or " + name2 + "?";
+
+	}
 	else
-		alert("Please key in something!");
+		result1.innerHTML = "Please key in something!";
+}
+
+function getResult2() {
+    var text = document.getElementById("nameInput").value.trim();
+	var nameList = text.split("\n");
+	var result1 = document.getElementById("result2");
+	result1.innerHTML = "";
+	if(text.length > 0)
+	{
+		var name1 = nameList[Math.floor(Math.random() * nameList.length)];
+		var name2 = nameList[Math.floor(Math.random() * nameList.length)];
+		while(name2 == name1)
+			name2 = nameList[Math.floor(Math.random() * nameList.length)];
+		result1.innerHTML = "What would you rather watch for the rest of your life: \"" + name1 + "\" or \"" + name2 + "\"?";
+
+	}
+	else
+		result1.innerHTML = "Please key in something!";
 }
