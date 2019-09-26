5021f56fa6bc46e22d6f2e93ef9613d205841c92
Neo Kai Neng <neo.kai.neng@gmail.com>
2019-09-26 16:26:20 +0800

    added local file viewer

--- index.js
+++ index.js
@@ -98,3 +98,32 @@ function getResult3() {
 /* on how to load csv file, add in here or another external js file
 https://stackoverflow.com/questions/24172045/automatically-import-a-local-csv-file-with-javascript-or-jquery/24172885#24172885
 */
+window.onload = function () { 
+ //Check the support for the File API support 
+ if (window.File && window.FileReader && window.FileList && window.Blob) {
+    var fileSelected = document.getElementById('txtfiletoread');
+    fileSelected.addEventListener('change', function (e) { 
+         //Set the extension for the file 
+         var fileExtension = /text.*/; 
+         //Get the file object 
+         var fileTobeRead = fileSelected.files[0];
+        //Check of the extension match 
+         if (fileTobeRead.type.match(fileExtension)) { 
+             //Initialize the FileReader object to read the 2file 
+             var fileReader = new FileReader(); 
+             fileReader.onload = function (e) { 
+                 var fileContents = document.getElementById('filecontents'); 
+                 fileContents.innerText = fileReader.result; 
+             } 
+             fileReader.readAsText(fileTobeRead); 
+         } 
+         else { 
+             alert("Please select text file"); 
+         }
+ 
+    }, false);
+ } 
+ else { 
+     alert("Files are not supported"); 
+ } 
+}
