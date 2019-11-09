5343cb94235b0becf074e12cd9c41a2e80767e80
Kaineng Neo <neo.kai.neng@gmail.com>
2019-11-09 11:17:58 +0800

    Update timeknots.js

--- timeknots.js
+++ timeknots.js
@@ -199,18 +199,18 @@ var TimeKnots = {
       svg.append("text")
          .text(startString).style("font-size", "70%").style("color", "white")
          .attr("x", function(d){if(cfg.horizontalLayout){return d3.max([0, (margin-this.getBBox().width/2)])} return Math.floor(this.getBBox().width/2)})
-         .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return margin+this.getBBox().height/2});
+         .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return margin+this.getBBox().height/2+10});
 
       svg.append("text")
          .text(endString).style("font-size", "70%").style("color", "white")
          .attr("x", function(d){if(cfg.horizontalLayout){return  cfg.width -  d3.max([this.getBBox().width, (margin+this.getBBox().width/2)])} return Math.floor(this.getBBox().width/2)})
-         .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return cfg.height-margin+this.getBBox().height/2})
+         .attr("y", function(d){if(cfg.horizontalLayout){return Math.floor(cfg.height/2+(margin+this.getBBox().height))}return cfg.height-margin+this.getBBox().height/2+10})
     }
 
 
     svg.on("mousemove", function(){
         tipPixels = parseInt(tip.style("height").replace("px", ""));
-    return tip.style("top", (d3.event.pageY)+"px").style("left",(d3.event.pageX)+"px");}) //top and left are aligned wrongly
+    return tip.style("top", (d3.event.pageY-tipPixels-margin-200)+"px").style("left",(d3.event.pageX+20-200)+"px");}) //top and left are aligned wrongly
     .on("mouseout", function(){return tip.style("opacity", 0).style("top","0px").style("left","0px");});
   }
 }
