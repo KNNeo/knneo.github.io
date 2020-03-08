e16de15dff4777da3b0a25784cebcb0887dc7b0a
Neo Kai Neng <neo.kai.neng@gmail.com>
2020-03-08 11:48:04 +0800

    add klassic note table html and data

--- would-you-rather/would-you-rather.js
+++ would-you-rather/would-you-rather.js
@@ -1,28 +1,3 @@
-function searchFunction() {
-	// Declare starting variables 
-	var input = document.getElementById("dbInput");
-	var filter = input.value.toUpperCase();
-	var table = document.getElementById("dbTable");
-	var trs = table.tBodies[0].getElementsByTagName("tr");
-	// Loop through rows
-	for (var i = 1; i < trs.length; i++) {
-		// Define the cells
-		var tds = trs[i].getElementsByTagName("td");
-		// hide the row
-		trs[i].style.display = "none";
-		// loop through row cells
-		for (var i2 = 0; i2 < tds.length; i2++) {
-			// if there's a match
-			if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
-			// show the row
-			trs[i].style.display = "";
-			// skip to the next row
-			continue;
-			}
-		}
-	}
-}
-
 function getResult(module) {
 	var categoryName = module.innerHTML;
     var text = document.getElementById("nameInput").value.trim();
