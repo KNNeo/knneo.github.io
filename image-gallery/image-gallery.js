7d57389c1fbe29ec15d611a554c9ee17d0564ed7
Neo Kai Neng <neo.kai.neng@gmail.com>
2020-04-26 20:26:24 +0800

    add data, allow scroll on desktop

--- image-gallery/image-gallery.js
+++ image-gallery/image-gallery.js
@@ -1,18 +1,48 @@
 //array containing all gallery info
 //[sortOrder,directory,orientation,name]
-var imgArray = [
+let imgArray = [
 [0,'','',''],
-[1, 'images/SELFSCAN_20140111_0002','portrait', 'Kotobuki Minako'],
-[2, 'images/SELFSCAN_20140111_0005','portrait', 'Hikasa Youko'],
-[3, 'images/SELFSCAN_20140111_0007','landscape','Tomatsu Haruka'],
-[4, 'images/SELFSCAN_20140111_0009','landscape','Taketatsu Ayana'],
-[5, 'images/SELFSCAN_20140111_0010','portrait', 'Taketatsu Ayana'],
-[6, 'images/SELFSCAN_20140111_0011','portrait', 'Taketatsu Ayana'],
-[7, 'images/SELFSCAN_20140307_0001','portrait', 'Horie Yui'],
-[8, 'images/SELFSCAN_20140311_0002','landscape','Satou Satomi'],
-[9, 'images/SELFSCAN_20140324_0001','portrait', 'Horie Yui'],
-[10,'images/SELFSCAN_20140324_0002','portrait', 'Toyosaki Aki'],
-[11,'images/SELFSCAN_20140424_0002','portrait', 'Kotobuki Minako'],
+[1, 'SELFSCAN_20140111_0002','portrait', 'Kotobuki Minako'],
+[2, 'SELFSCAN_20140111_0005','portrait', 'Hikasa Youko'],
+[3, 'SELFSCAN_20140111_0007','landscape','Tomatsu Haruka'],
+[4, 'SELFSCAN_20140111_0009','landscape','Taketatsu Ayana'],
+[5, 'SELFSCAN_20140111_0010','portrait', 'Taketatsu Ayana'],
+[6, 'SELFSCAN_20140111_0011','portrait', 'Taketatsu Ayana'],
+[7, 'SELFSCAN_20140307_0001','portrait', 'Horie Yui'],
+[8, 'SELFSCAN_20140311_0002','landscape','Satou Satomi'],
+[9, 'SELFSCAN_20140324_0001','portrait', 'Horie Yui'],
+[10,'SELFSCAN_20140324_0002','portrait', 'Toyosaki Aki'],
+[11,'SELFSCAN_20140424_0002','portrait', 'Kotobuki Minako'],
+[12,'SELFSCAN_20140529_0001','landscape','sphere'],
+[13,'SELFSCAN_20140719_0001','portrait', 'Toyosaki Aki'],
+[14,'SELFSCAN_20140823_0001','portrait', 'Amamiya Sora'],
+[15,'SELFSCAN_20141117_0001','portrait', 'Toyosaki Aki'],
+[16,'SELFSCAN_20150228_0005','landscape','sphere'],
+[17,'SELFSCAN_20150415_0001','portrait', 'Kotobuki Minako'],
+[18,'SELFSCAN_20150526_0001','portrait', 'TrySail'],
+[19,'SELFSCAN_20150625_0002','portrait', 'Toyosaki Aki'],
+[20,'SELFSCAN_20150925_0001','portrait', 'Kotobuki Minako'],
+[21,'SELFSCAN_20151001_0001','portrait', 'Tomatsu Haruka'],
+[22,'SELFSCAN_20160204_0001','portrait', 'Taketatsu Ayana'],
+[23,'SELFSCAN_20160227_0005','portrait', 'Tomatsu Haruka'],
+[24,'SELFSCAN_20151001_0001','portrait', 'Tomatsu Haruka'],
+[25,'SELFSCAN_20160402_0001','portrait', 'Toyosaki Aki'],
+[26,'SELFSCAN_20160531_0001','landscape','TrySail'],
+[27,'SELFSCAN_20160602_0003','portrait', 'TrySail'],
+[11,'SELFSCAN_20160629_0003','portrait', 'Kotobuk Minako'],
+[11,'SELFSCAN_20160629_0004','landscape','Toyosaki Aki'],
+[11,'SELFSCAN_20160629_0006','portrait', 'Toyosaki Aki'],
+[11,'SELFSCAN_20160629_0007','landscape','Toyosaki Aki'],
+[11,'SELFSCAN_20160629_0008','landscape','Toyosaki Aki'],
+[11,'SELFSCAN_20160629_0010','portrait', 'Toyosaki Aki'],
+[11,'SELFSCAN_20160629_0011','portrait', 'Toyosaki Aki'],
+[11,'SELFSCAN_20160905_0001','portrait', 'Toyosaki Aki'],
+[11,'SELFSCAN_20170510_0001','portrait', 'Natsukawa Shiina'],
+[27,'SELFSCAN_20170602_0002','portrait', 'TrySail'],
+[11,'SELFSCAN_20170610_0001','portrait', 'Toyosaki Aki'],
+[11,'SELFSCAN_20170728_0005','portrait', 'Uesaka Sumire'],
+[27,'SELFSCAN_20170728_0008','portrait', 'TrySail'],
+[11,'SELFSCAN_20170908_0001','landscape','Natsukawa Shiina'],
 [999,'','','']
 ];
 
@@ -29,7 +59,7 @@ function renderGallery(array) {
 		profileBoxImgHTML.classList.add('profile-box-img');
 		let imgHTML = document.createElement('IMG');
 		imgHTML.classList.add(img[2]);
-		imgHTML.alt = img[1] + '.jpg';
+		imgHTML.alt = 'images/' + img[1] + '.jpg';
 		imgHTML.title = img[3];
 		profileBoxImgHTML.appendChild(imgHTML);
 		profileBoxHTML.appendChild(profileBoxImgHTML);
@@ -89,6 +119,23 @@ for(let label of labelArray)
 }
 
 //add event listeners to tickboxes
+document.getElementById('SelectAll').addEventListener('click', function () {
+	if(document.getElementById('SelectAll').checked == true)
+	{
+		for (let label of document.getElementById('name').getElementsByTagName('input'))
+		{
+			label.checked = true;
+		}
+	}
+	else
+	{
+		for (let label of document.getElementById('name').getElementsByTagName('input'))
+		{
+			label.checked = false;
+		}
+	}
+	renderFilter();
+});
 for (let tickbox of document.getElementsByTagName('label'))
 {
 	tickbox.addEventListener('click',renderFilter);
@@ -119,6 +166,13 @@ function renderFilter() {
 document.getElementById('viewer').addEventListener('click', function() {
 	document.getElementById('viewer').style.display = 'none';
 	document.getElementById('viewer').innerHTML = '';
+});
+
+//allow scroll on desktop
+document.getElementById("imgGallery").addEventListener("wheel", function(e) {
+    e.preventDefault();
+    document.getElementsByClassName('profile-category')[0].scrollLeft -= e.wheelDelta*4;
+	return;
 });
 
 //prevent right click events
