a97f817fa58b070380d8aa9da39399661730be92
Neo Kai Neng <neo.kai.neng@gmail.com>
2020-08-03 23:12:53 +0800

    Update HallOfFame_Ranking_ByArtistType.sql

--- klassic-note-table/sql/HallOfFame_Ranking_ByArtistType.sql
+++ klassic-note-table/sql/HallOfFame_Ranking_ByArtistType.sql
@@ -1,2 +1,9 @@
+--every year
 select distinct Ranking.KNYEAR, Ranking.RankNo, Ranking.SortOrder, Song.SongTitle, Artist.ArtistTitle, Artist.ArtistCode
 from Ranking join Song on Ranking.KNID = Song.KNID join Artist on Song.ArtistTitle = Artist.ArtistTitle where Ranking.KNYEAR = 2019
+
+--overall
+select a.ArtistCode, count(a.ArtistCode) from (
+select distinct Ranking.KNYEAR, Ranking.RankNo, Ranking.SortOrder, Song.SongTitle, Artist.ArtistTitle, Artist.ArtistCode
+from Ranking join Song on Ranking.KNID = Song.KNID join Artist on Song.ArtistTitle = Artist.ArtistTitle
+)a group by a.ArtistCode
