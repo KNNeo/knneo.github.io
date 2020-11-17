a3f8d74c16daebd025a3467c927aac1aac956ef8
Neo Kai Neng <neo.kai.neng@gmail.com>
2020-11-17 20:55:15 +0800

    Update DatabaseIntegrityChecks.sql

--- klassic-note-table/sql/DatabaseIntegrityChecks.sql
+++ klassic-note-table/sql/DatabaseIntegrityChecks.sql
@@ -1,11 +1,15 @@
---Every Song must have an associated Release, with details to map
-select distinct Song.ReleaseArtistTitle, Song.ReleaseTitle from Song left join Release on Song.ReleaseTitle = Release.ReleaseTitle and Song.ReleaseArtistTitle = Release.ReleaseArtistTitle where Release.ReleaseID is NULL order by Song.ReleaseArtistTitle, Song.ReleaseTitle
-
+--ANY QUERY BELOW MUST RETURN EMPTY TABLE IN ORDER TO PASS--
+--PATCHING TO BE DONE ON RELEASE TABLE FIRST UNLESS OTHERWISE--
+---------------------------------------------------------------
 --Every Song must have an associated Artist, with details to map
 select distinct Song.ArtistTitle from Song left join Artist on Song.ArtistTitle = Artist.ArtistTitle where Artist.ArtistID is NULL order by Song.ArtistTitle
 
+--Every Song must have an associated Release, with details to map
+select distinct Song.ReleaseArtistTitle, Song.ReleaseTitle from Song left join Release on replace(replace(replace(Song.ReleaseTitle,' Disc 1',''),' Disc 2',''),' Disc 3','') = Release.ReleaseTitle and Song.ReleaseArtistTitle = Release.ReleaseArtistTitle where Release.ReleaseID is NULL order by Song.ReleaseArtistTitle, replace(replace(replace(Song.ReleaseTitle,' Disc 1',''),' Disc 2',''),' Disc 3','')
+
 --Every ParentArtist must be in Song list
 --[[ParentArtist as of Artist table can be descriptive of its members and therefore not in Song list as ArtistTitle eg. STARTails]]--
+select distinct Song.ParentArtist from Song left join Artist on Song.ParentArtist = Artist.ParentArtist or Song.ParentArtist = Artist.ArtistTitle where Song.ParentArtist <> 'V.A.' and Artist.ArtistID is NULL order by Song.ParentArtist
 
 --Every Award must have associated KNID based on Song and ArtistTitle
 --[[See Excel: Awards Table]]--
@@ -21,5 +25,9 @@ select ROW_NUMBER () OVER ( order by Song.DateCreated ) as NewArtistID, Artist.A
 
 --Database Reconstruction Plan--
 --Assumptions:
+/*
+(1) Song order before including KN2017 is by date created, else follow Song Integrity
+
+*/
 
 
