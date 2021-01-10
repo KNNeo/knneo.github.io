1ddcc032bd984de1ac811d0b3d990091e92d12c5
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-01-10 15:50:59 +0800

    fix search domain

--- blogspot/blog.js
+++ blogspot/blog.js
@@ -94,7 +94,7 @@ function addObjects() {
 	if(document.getElementById('CustomBlogSearch') == null) {
 		let search = document.createElement('div');
 		search.id = 'CustomBlogSearch';
-		search.innerHTML = '<div class="widget-content"><div id="_form"><form action="https://knwebreports.blogspot.com/search" class="gsc-search-box" target="_top"><div cellpadding="0" cellspacing="0" class="gsc-search-box"><div><div><span class="gsc-input"><input autocomplete="off" class="gsc-input" name="q" size="10" title="search" type="text" value=""></span><span class="gsc-search-button" style="display: none;"><input class="gsc-search-button" name="max-results" title="search" type="submit" value="5"></span></div></div></div></form></div></div>';
+		search.innerHTML = '<div class="widget-content"><div id="_form"><form action="' + window.location.origin + '/search" class="gsc-search-box" target="_top"><div cellpadding="0" cellspacing="0" class="gsc-search-box"><div><div><span class="gsc-input"><input autocomplete="off" class="gsc-input" name="q" size="10" title="search" type="text" value=""></span><span class="gsc-search-button" style="display: none;"><input class="gsc-search-button" name="max-results" title="search" type="submit" value="5"></span></div></div></div></form></div></div>';
 		
 		if(document.getElementsByClassName('header-outer').length > 0)
 			document.getElementsByClassName('header-outer')[0].appendChild(search);
