4a6df10b78d84d348fa9a252c432f5b1a75a5218
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-01-24 22:55:31 +0800

    Update blog.js

--- blogspot/blog.js
+++ blogspot/blog.js
@@ -171,7 +171,8 @@ function reduceResults() {
 	//Exceptions in order: single posts, previews, pages
     if ((window.location.href.includes(window.location.origin + '/20') && window.location.href.includes('.html')) || 
 		window.location.href.includes(window.location.origin + '/b/blog-preview') || 
-		window.location.href.includes(window.location.origin + '/p/'))
+		window.location.href.includes(window.location.origin + '/p/') ||
+		window.location.href.includes('LINQPad'))
 		return;
 	//Remove content
 	for (var footer of document.getElementsByClassName('post-footer-line-2')) {
