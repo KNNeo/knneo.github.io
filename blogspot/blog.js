745e11f5e6c86d76902245a92ba932ad5910e405
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-03-14 17:10:24 +0800

    update addhashtags (blogspot)

--- blogspot/blog.js
+++ blogspot/blog.js
@@ -690,7 +690,9 @@ function addHashtags() {
 	var hashtags = [];
 	for(var topic of document.getElementsByClassName("anime"))
 	{
-		if(topic.parentElement.style.display == "none") continue;
+		//if last 2 characters do not render a number, do not add
+		var numeric = parseInt(topic.id.slice(-2)) || 0;
+		if(numeric == 0) continue;
 		hashtags.push({
 			tag: topic.id.substring(0,topic.id.length-2), 
 			target: topic.id
