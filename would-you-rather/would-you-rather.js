2bb8650f34ef90569f575ed1767537bd7fb72089
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-04-16 22:34:59 +0800

    Update would-you-rather.js

--- would-you-rather/would-you-rather.js
+++ would-you-rather/would-you-rather.js
@@ -1,67 +1,23 @@
 let mirrorFeatures = ["cutest","coolest","most beautiful","funniest","sexiest"];
+let nameList = [];
 let newList = [];
 
 function getResultFromNames(module) {
 	let categoryName = module.innerHTML;
     let text = document.getElementById("nameInput").value.trim();
-	let nameList = text.split("\n");
 	let result = document.getElementById("result1");
+	nameList = text.split("\n");
 	if(!result.innerHTML.includes('flexi-input')) result.innerHTML = "";
 	//if(document.getElementById("nameInput").name !== "Load People Name Preset") result.innerHTML = "Wrong list loaded!";
 	if(nameList.length < 4) result.innerHTML = "List is too short! Minimum 4 names!";
 	else if(text.length > 0)
 	{
 		newList = [];
-		if(categoryName == "Dating")	
-			result.innerHTML = "Would you rather go on a date with \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if(categoryName == "F**k Marry Kill")
-			result.innerHTML = "Who would you rather f**k, marry or kill: \"" + generateName(nameList) + "\", \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if(categoryName == "Save")	
-			result.innerHTML = "Who would you rather save first when both of them fell into the river \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"? (Assume both cannot swim)";
-		else if(categoryName == "Mirror")
-		{
-			let mirrorFeature = mirrorFeatures[Math.floor(Math.random() * mirrorFeatures.length)];
-			result.innerHTML = "Mirror, mirror, on the wall; Who is the " + mirrorFeature + " of them all? \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		}
-		else if (categoryName == "Messager")
-			result.innerHTML = "Would you rather exchange numbers and message every day with \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Hair")
-			result.innerHTML = "Would you prefer long-haired \"" + generateName(nameList) + "\" or short-haired \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Last Round")
-			result.innerHTML = "It's the last round of a board game. Who would you rather let win? \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"? (Assume both are tied)";
-		else if (categoryName == "Bump")
-			result.innerHTML = "Would you rather do a chest bump with \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Sweet Talk")
-			result.innerHTML = "Would you rather say sweet nothings all day with \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Sweet Talk")
-			result.innerHTML = "Would you rather say sweet nothings all day with \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Meal")
-			result.innerHTML = "Would you rather treat \"" + generateName(nameList) + "\" to a meal at the restaurant or have a cheap takeaway meal with \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Housewife")
-			result.innerHTML = "If \"" + generateName(nameList) + "\" came to you after your work from home and asked \"Would you rather have dinner, have a bath or me?\", what would you prefer?";
-		else if (categoryName == "Day/Night")
-			result.innerHTML = "Would you rather spend the day with \"" + generateName(nameList) + "\" or spend the night with \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Minutes/Seconds")
-			result.innerHTML = "Would you rather have 2 minutes with \"" + generateName(nameList) + "\" or spend 2 seconds with \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "BFF")
-			result.innerHTML = "Would you rather be BFFs with \"" + generateName(nameList) + "\" (and never be able to marry her) and/or marry \"" + generateName(nameList) + "\" (and let her be your sole partner forever)?";
-		else if (categoryName == "Kidnap")
-			result.innerHTML = "Who would you rather be kidnap for 24 hours and do anything with but nothing will be remembered? \"" + generateName(nameList) + "\" or \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Betrayal")
-			result.innerHTML = "Would you rather stay with your current date \"" + generateName(nameList) + "\" or betray her and go date \"" + generateName(nameList) + "\"?";
-		else if (categoryName == "Harem")
-			result.innerHTML = "It's a harem!! Would you rather date \"" + generateName(nameList) + "\" who is your childhood friend, or \"" + generateName(nameList) + "\" who is your long lost step sibling, or \"" + generateName(nameList) + "\" who landed on top of you by accident, or \"" + generateName(nameList) + "\" who suddenly asked you out to confess to you?";
-		else if (categoryName == "Affair")
-			result.innerHTML = "Would you rather \"" + generateName(nameList) + "\" be your wife while you have an affair with \"" + generateName(nameList) + "\"? Or vice versa?";
-		else if (categoryName == "Island")
-			result.innerHTML = "You and " + generateName(nameList) + " are stuck in an uninhabited island. Would you rather escape in a lifeboat only enough for one, try to call for rescue while waiting together, or procreate and start a new life?";
-		else if (categoryName == "Hug/Kiss")
-			result.innerHTML = "Would you rather receive a hug from " + generateName(nameList) + " or receive a kiss on the cheek from " + generateName(nameList) + "?";	
-		else if(categoryName == "Custom")
+		if(categoryName == "Custom")
 		{
 			if(result.innerHTML.includes('flexi-name')) {
-				result.getElementsByClassName('flexi-name')[0].innerText = generateName(nameList);
-				result.getElementsByClassName('flexi-name')[1].innerText = generateName(nameList);
+				result.getElementsByClassName('flexi-name')[0].innerText = generateName();
+				result.getElementsByClassName('flexi-name')[1].innerText = generateName();
 			}
 			else {				
 				let holder = document.createElement('div');
@@ -76,7 +32,7 @@ function getResultFromNames(module) {
 				
 				let randomName1 = document.createElement('span');
 				randomName1.classList.add('flexi-name');
-				randomName1.innerText = generateName(nameList);
+				randomName1.innerText = generateName();
 				holder.appendChild(randomName1);
 							
 				let text2 = document.createElement('span');
@@ -89,7 +45,7 @@ function getResultFromNames(module) {
 				
 				let randomName2 = document.createElement('span');
 				randomName2.classList.add('flexi-name');
-				randomName2.innerText = generateName(nameList);
+				randomName2.innerText = generateName();
 				holder.appendChild(randomName2);
 				
 				let text3 = document.createElement('span');
@@ -98,14 +54,64 @@ function getResultFromNames(module) {
 				
 				result.appendChild(holder);
 			}
-			
 		}
+		else
+			result.innerHTML = displayOutput(categoryName);
 	}
 	else
 		result.innerHTML = "Please key in something!";
 }
 
-function generateName(nameList) {
+function displayOutput(categoryName) {
+	if(categoryName == "Dating")
+		return "Would you rather go on a date with \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if(categoryName == "F**k Marry Kill")
+		return "Who would you rather f**k, marry or kill: \"" + generateName() + "\", \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if(categoryName == "Save")	
+		return "Who would you rather save first when both of them fell into the river \"" + generateName() + "\" or \"" + generateName() + "\"? (Assume both cannot swim)";
+	else if(categoryName == "Mirror")
+	{
+		let mirrorFeature = mirrorFeatures[Math.floor(Math.random() * mirrorFeatures.length)];
+		return "Mirror, mirror, on the wall; Who is the " + mirrorFeature + " of them all? \"" + generateName() + "\" or \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	}
+	else if (categoryName == "Messager")
+		return "Would you rather exchange numbers and message every day with \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if (categoryName == "Hair")
+		return "Would you prefer long-haired \"" + generateName() + "\" or short-haired \"" + generateName() + "\"?";
+	else if (categoryName == "Last Round")
+		return "It's the last round of a board game. Who would you rather let win? \"" + generateName() + "\" or \"" + generateName() + "\"? (Assume both are tied)";
+	else if (categoryName == "Bump")
+		return "Would you rather do a chest bump with \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if (categoryName == "Sweet Talk")
+		return "Would you rather say sweet nothings all day with \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if (categoryName == "Sweet Talk")
+		return "Would you rather say sweet nothings all day with \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if (categoryName == "Meal")
+		return "Would you rather treat \"" + generateName() + "\" to a meal at the restaurant or have a cheap takeaway meal with \"" + generateName() + "\"?";
+	else if (categoryName == "Housewife")
+		return "If \"" + generateName() + "\" came to you after your work from home and asked \"Would you rather have dinner, have a bath or me?\", what would you prefer?";
+	else if (categoryName == "Day/Night")
+		return "Would you rather spend the day with \"" + generateName() + "\" or spend the night with \"" + generateName() + "\"?";
+	else if (categoryName == "Minutes/Seconds")
+		return "Would you rather have 2 minutes with \"" + generateName() + "\" or spend 2 seconds with \"" + generateName() + "\"?";
+	else if (categoryName == "BFF")
+		return "Would you rather be BFFs with \"" + generateName() + "\" (and never be able to marry her) and/or marry \"" + generateName() + "\" (and let her be your sole partner forever)?";
+	else if (categoryName == "Kidnap")
+		return "Who would you rather be kidnap for 24 hours and do anything with but nothing will be remembered? \"" + generateName() + "\" or \"" + generateName() + "\"?";
+	else if (categoryName == "Betrayal")
+		return "Would you rather stay with your current date \"" + generateName() + "\" or betray her and go date \"" + generateName() + "\"?";
+	else if (categoryName == "Harem")
+		return "It's a harem!! Would you rather date \"" + generateName() + "\" who is your childhood friend, or \"" + generateName() + "\" who is your long lost step sibling, or \"" + generateName() + "\" who landed on top of you by accident, or \"" + generateName() + "\" who suddenly asked you out to confess to you?";
+	else if (categoryName == "Affair")
+		return "Would you rather \"" + generateName() + "\" be your wife while you have an affair with \"" + generateName() + "\"? Or vice versa?";
+	else if (categoryName == "Island")
+		return "You and " + generateName() + " are stuck in an uninhabited island. Would you rather escape in a lifeboat only enough for one, try to call for rescue while waiting together, or procreate and start a new life?";
+	else if (categoryName == "Hug/Kiss")
+		return "Would you rather receive a hug from " + generateName() + " or receive a kiss on the cheek from " + generateName() + "?";
+	return;
+}
+
+function generateName() {
 	let newName = nameList[Math.floor(Math.random() * nameList.length)];
 	
 	while(newList.indexOf(newName) > -1)
@@ -132,7 +138,7 @@ function toggleCensor() {
 function getResultFromTitles(module) {
 	let categoryName = module.innerHTML;
     let text = document.getElementById("nameInput").value.trim();
-	let nameList = text.split("\n");
+	nameList = text.split("\n");
 	let result = document.getElementById("result2");
 	result.innerHTML = "";
 	if(document.getElementById("nameInput").name !== "Load Show Titles Preset") result.innerHTML = "Wrong list loaded!";
