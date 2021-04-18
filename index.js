930f3eaf11d7691dfd27956fa480f2b0ed2287a6
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-04-18 11:28:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const isMobile = function() {
 //*dark mode check*//
 if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleDarkMode();
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', toggleDarkMode);
-document.getElementById('darkmode').addEventListener('click', toggleDarkMode);
+if(document.getElementById('darkmode') != null)
+	document.getElementById('darkmode').addEventListener('click', toggleDarkMode);
 
 function toggleDarkMode() {
 	if(document.getElementsByTagName('html')[0].classList.contains('darked'))
