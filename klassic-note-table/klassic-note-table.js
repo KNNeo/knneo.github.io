f7495a529b8d12dbe781387fb8a244e35b0b2877
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-05-22 15:33:06 +0800

    escape to clear fields (klassic-note-table)

--- klassic-note-table/klassic-note-table.js
+++ klassic-note-table/klassic-note-table.js
@@ -113,12 +113,13 @@ function generateSearch(filters) {
 			columnInput.style.display = document.getElementById('tickbox' + column) != null && !document.getElementById('tickbox' + column).checked ? 'none' : '';
 			
 			columnInput.addEventListener('keyup', function(event) {
-				// Number 13 is the "Enter" key on the keyboard
-				if (event.keyCode === 13) {
-					// Cancel the default action, if needed
+				if (event.keyCode === 13) { // "Enter"
 					event.preventDefault();
-					// Trigger the button element with a click
 					document.getElementById("dbSubmitButton").click(this);
+				}
+				if (event.keyCode === 27) { // "Esc"
+					event.preventDefault();
+					document.getElementById("dbClearButton").click(this);
 				}
 			});
 			
