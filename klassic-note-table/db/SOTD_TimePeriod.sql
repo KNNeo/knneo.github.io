dc7c83f4904fb1d08a67b6a53e869ec7ee146d6c
Neo Kai Neng <neo.kai.neng@gmail.com>
2021-12-30 10:35:45 +0800

    Update SOTD_TimePeriod.sql

--- klassic-note-table/db/SOTD_TimePeriod.sql
+++ klassic-note-table/db/SOTD_TimePeriod.sql
@@ -1,4 +1,5 @@
 select SongTitle, ArtistTitle, ArtistTitle || ' - ' || SongTitle, count(SongTitle) from SOTD 
 where date between 20210106 and 20211226 --date range
+and IsPastYear = false --nostalgia flag
 group by SongTitle, ArtistTitle having Count(SongTitle) > 0 --min count
 order by count(SongTitle) desc
