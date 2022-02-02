1bfd26d41bc2d3a3fadc39fd6641f514f9d0b863
Neo Kai Neng <neo.kai.neng@gmail.com>
2022-02-02 11:44:53 +0800

    fix search buttons (web)

--- klassic-note-web/index.js
+++ klassic-note-web/index.js
@@ -424,6 +424,7 @@ function copySearch() {
 function clearSearch() {
 	document.getElementById('search').value = '';
 	document.getElementById('tab-homepage').style.display = '';
+	document.getElementById('search-buttons').style.display = 'none';
 	document.getElementById('tab-buttons').style.display = 'none';
 	for(let tab of document.getElementsByClassName('module'))
 	{
@@ -434,6 +435,7 @@ function clearSearch() {
 		tab.classList.add('hidden');
 	}
 	renderHomepage();
+	document.getElementById('search').style.width = '100%';
 }
 
 function onChangeOption() {
@@ -526,6 +528,7 @@ function generateLayout(contents) {
 	// console.log('generateLayout', contents);
 	document.getElementById('tab-homepage').style.display = 'none';
 	document.getElementById('tab-buttons').style.display = '';
+	document.getElementById('search-buttons').style.display = '';
 	
 	updateSearch(contents);
 	generatePlayer(contents);
