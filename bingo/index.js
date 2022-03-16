b110998b842b1313d3358fb2f462b159e949235c
Neo Kai Neng <neo.kai.neng@gmail.com>
2022-03-16 22:21:46 +0800

    add bingo conditions, styling, generate

--- bingo/index.js
+++ bingo/index.js
@@ -1,5 +1,14 @@
 //--DEFAULT SETTINGS--//
-
+const combinations = [
+	{
+		"name": "W",
+		"selected": [1,5,6,10,11,13,15,16,18,20,22,24],
+	},
+	{
+		"name": "E",
+		"selected": [1,2,3,4,5,6,11,12,13,14,15,16,21,22,23,24,25],
+	},
+];
 
 //--COMMON EVENTS--//
 //on startup
@@ -14,7 +23,9 @@ const smallScreen = window.innerWidth <= 640;
 window.addEventListener('load', startup);
 
 function startup() {
+	window['combination'] = 'W';
 	renderList();
+	renderActions();
 }
 
 function renderList() {
@@ -40,8 +51,8 @@ function renderList() {
 	// console.log('numbers', numbers);
 	
 	let table = document.createElement('table');
+	table.id = 'card';
 	table.classList.add('box');
-	table.classList.add('shadowed');
 	
 	let tbody = document.createElement('tbody');
 	
@@ -53,9 +64,12 @@ function renderList() {
 		{
 			let td = document.createElement('td');
 			td.classList.add('square');
+			td.classList.add('large-font');
+			td.classList.add('shadowed');
+			td.setAttribute('data-id', m*5+n+1);
 			td.innerText = numbers[m*5+n];
 			td.addEventListener('click', function() {
-				this.style.backgroundColor = this.style.backgroundColor == '' ? 'lightgray' : '';
+				this.classList.toggle('selected');
 			});
 			tr.appendChild(td);
 		}
@@ -64,4 +78,48 @@ function renderList() {
 	
 	table.appendChild(tbody);
 	document.querySelector('.list').appendChild(table);	
+}
+
+function renderActions() {
+	let generate = document.createElement('div');
+	generate.id = 'generate';
+	generate.classList.add('shadowed');
+	generate.classList.add('large-font');
+	generate.innerText = 'Generate';
+	generate.addEventListener('click', renderList);
+	document.querySelector('.menu').appendChild(generate);
+
+	let bingo = document.createElement('div');
+	bingo.id = 'bingo';
+	bingo.classList.add('shadowed');
+	bingo.classList.add('large-font');
+	bingo.innerText = 'Bingo';
+	bingo.addEventListener('click', checkBingo);
+	document.querySelector('.menu').appendChild(bingo);
+}
+
+function checkBingo() {
+	let card = document.querySelector('#card');
+	let selectedIndices = Array.from(card.querySelectorAll('.selected')).map(cell => parseInt(cell.getAttribute('data-id')));
+	console.log(JSON.stringify(selectedIndices));
+	
+	let target = combinations.filter(c => c.name == 'W')[0];
+	let count = 0;
+	for(let number of target.selected)
+	{
+		if(selectedIndices.indexOf(number) >= 0)
+			count++;
+	}
+	
+	if(count == target.selected.length)
+	{
+		document.querySelector('#bingo').innerText = '✔️';
+		// console.log('bingo');
+	}
+	else
+	{
+		document.querySelector('#bingo').innerText = '❌';
+		// console.log('not yet');
+	}
+	setTimeout(function() { document.querySelector('#bingo').innerText = 'Bingo'; }, 1000);
 }
