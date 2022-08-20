64b915cb95807bd17ae63c8e93120233deb2a5a3
Neo Kai Neng <neo.kai.neng@gmail.com>
2022-08-20 17:30:44 +0800

    Update Query_Artist_MostPopularSong.sql

--- klassic-note-web/db/Query_Artist_MostPopularSong.sql
+++ klassic-note-web/db/Query_Artist_MostPopularSong.sql
@@ -8,8 +8,8 @@
 --Compilations Featured
 ------------
 
-select s.KNID
-, s.SongTitle
+select SongTitle from (
+select s.SongTitle
 , (select count(*) from SOTD where sotd.KNID = s.KNID) as 'SOTD Mentions'
 , (select count(*) from SOTM where sotm.KNID = s.KNID) as 'SOTM Mentions'
 , (select count(*) from Award aw where aw.KNID = s.KNID) as 'Awards Nominated'
@@ -17,5 +17,15 @@ select s.KNID
 , (select count(*) from Ranking r where r.KNID = s.KNID) as 'Ranking Top 20'
 , (select count(*) from Ranking r where r.KNID = s.KNID and r.RankNo = 1) as 'Ranking Top 1'
 , (select count(*) from Compilation c where c.KNID = s.KNID and c.CompilationTitle <> 'GOLD') as 'Compilations Featured'
+, (
+(select count(*) from SOTD where sotd.KNID = s.KNID) +
+(select count(*) from SOTM where sotm.KNID = s.KNID) +
+(select count(*) from Award aw where aw.KNID = s.KNID) +
+(select count(*) from Award aw where aw.KNID = s.KNID and aw.isWinner = 1) +
+(select count(*) from Ranking r where r.KNID = s.KNID) +
+(select count(*) from Ranking r where r.KNID = s.KNID and r.RankNo = 1) +
+(select count(*) from Compilation c where c.KNID = s.KNID and c.CompilationTitle <> 'GOLD')
+) as 'Total'
 from Song s
-where s.ArtistTitle = 'Ryokuoushoku Shakai'
+where s.ArtistTitle = 'Ikimonogakari' and Total > 0 order by Total desc
+)
