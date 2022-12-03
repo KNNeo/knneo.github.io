a395b9ee10b7be097a985044dba987d5d8b53997
Neo Kai Neng <neo.kai.neng@gmail.com>
2022-12-03 12:00:34 +0800

    Update HallOfFame_NewArtists.sql

--- klassic-note-web/db/HallOfFame_NewArtists.sql
+++ klassic-note-web/db/HallOfFame_NewArtists.sql
@@ -1,12 +1,2 @@
 --List of New Artists
-select ArtistTitle from song where knyear = 2007 except select ArtistTitle from song where knyear < 2007
-select ArtistTitle from song where knyear = 2008 except select ArtistTitle from song where knyear <= 2007
-select ArtistTitle from song where knyear = 2009 except select ArtistTitle from song where knyear <= 2008
-select ArtistTitle from song where knyear = 2010 except select ArtistTitle from song where knyear <= 2009
-select ArtistTitle from song where knyear = 2011 except select ArtistTitle from song where knyear <= 2010
-select ArtistTitle from song where knyear = 2012 except select ArtistTitle from song where knyear <= 2011
-select ArtistTitle from song where knyear = 2013 except select ArtistTitle from song where knyear <= 2012
-select ArtistTitle from song where knyear = 2014 except select ArtistTitle from song where knyear <= 2013
-select ArtistTitle from song where knyear = 2015 except select ArtistTitle from song where knyear <= 2014
-select ArtistTitle from song where knyear = 2016 except select ArtistTitle from song where knyear <= 2015
-select ArtistTitle from song where knyear = 2017 except select ArtistTitle from song where knyear <= 2016
+select KNYEAR, group_concat(ArtistTitle, ' | ') from song s1 where ArtistTitle not in (select ArtistTitle from song s2 where s2.KNYEAR <= s1.KNYEAR - 1) group by KNYEAR
