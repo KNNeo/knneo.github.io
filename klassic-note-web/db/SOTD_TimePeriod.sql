4bc109ef733e09367fddaaccfd3598b3d426488b
Neo Kai Neng <neo.kai.neng@gmail.com>
2022-12-28 11:47:29 +0800

    Update SOTD_TimePeriod.sql

--- klassic-note-web/db/SOTD_TimePeriod.sql
+++ klassic-note-web/db/SOTD_TimePeriod.sql
@@ -1,6 +1,6 @@
 select SongTitle, ArtistTitle, ArtistTitle || ' - ' || SongTitle as 'DisplayValue', count(SongTitle) as 'Count' from SOTD 
---where date between 20220601 and 20220630 --date range, inclusive
-where Date like '202208%' --date range, same month
-and IsPastYear = 0 and IsShortPreview = 0 --nostalgia flag
+where date between 20220105 and 20221225 --date range, inclusive
+--where Date like '202212%' --date range, same month
+and IsShortPreview = 0 --not preview flag
 group by SongTitle, ArtistTitle having Count(SongTitle) > 0 --min count
 order by count(SongTitle) desc
