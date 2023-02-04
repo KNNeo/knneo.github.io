0b9024d879eb06fa4445765e9a3d878dd37194bc
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-02-04 22:28:55 +0800

    use global dark mode (klassic-note-table)

--- klassic-note-table/index.js
+++ klassic-note-table/index.js
@@ -543,8 +543,6 @@ function resetTable() {
 //--STARTUP--//
 function setup() {
 	initialize();
-	setDarkMode();
-	addDarkModeEvents();	
 	resetTable();
 	addDragAndDrop();
 	window.addEventListener('resize', resize);
@@ -596,50 +594,6 @@ function initialize() {
 		
 		if(this.checked) resetPresets();
 	});
-}
-
-//--DARK MODE--//
-function setDarkMode() {
-	let theme = Array.from(document.getElementsByTagName('meta')).filter(m => m.name == 'theme-color');
-	let themeColor = document.createElement('meta');
-	if(theme && theme.length > 0)
-	{
-		themeColor = theme[0];
-	}
-	else
-	{
-		themeColor = document.createElement('meta');
-		themeColor.name = 'theme-color';
-		document.head.appendChild(themeColor);
-	}
-	
-	if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleDarkMode();
-	themeColor.content = document.getElementsByTagName('html')[0].classList.contains('darked') ? 'black' : 'white';
-}
-
-function addDarkModeEvents() {
-	//assume supports dark mode
-	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', toggleDarkMode);
-	//assume page has button with id = darkmode
-	if(document.getElementById('darkmode') != null)
-		document.getElementById('darkmode').addEventListener('click', toggleDarkMode);
-	if(document.getElementById('darkmode') != null)
-		document.getElementById('darkmode').addEventListener('click', setColors);
-}
-
-function toggleDarkMode() {
-	let theme = Array.from(document.getElementsByTagName('meta')).filter(m => m.name == 'theme-color');
-	let themeColor = theme[0];
-	if(document.getElementsByTagName('html')[0].classList.contains('darked')) //parent class of each page
-	{
-		document.getElementsByTagName('html')[0].classList.remove('darked');
-		themeColor.content = 'white';
-	}
-	else
-	{
-		document.getElementsByTagName('html')[0].classList.add('darked');
-		themeColor.content = 'black';
-	}
 }
 
 //--TIMELINE--//
