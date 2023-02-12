6c84e3c4435f2f30aa49e16669713469573a99dd
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-02-12 18:41:05 +0800

    add horizontal timeline, with gaps (profile-list-v4)

--- profile-list/v4/index.js
+++ profile-list/v4/index.js
@@ -225,8 +225,8 @@ function generateWantedListEntry(id, autoAdd = []) {
 function loadTimeline(width = 2500) {
 	if(document.querySelector('#timeline') == null) return;
 	document.querySelector('#timeline').innerHTML = '';
-	generateVerticalTimeline('timeline', window['timelineDOBlist'].filter(prof => !prof.date.startsWith('????')), null, '30vh');
-	// generateHorizontalTimeline('timeline', window['timelineDOBlist'].filter(prof => !prof.date.startsWith('????')), width, '140px');
+	// generateVerticalTimeline('timeline', window['timelineDOBlist'].filter(prof => !prof.date.startsWith('????')), null, '30vh');
+	generateHorizontalTimeline('timeline', window['timelineDOBlist'].filter(prof => !prof.date.startsWith('????')), width, '140px');
 	addTimelineEvents(false);
 }
 
--- profile-list/v4/timeline.js
+++ profile-list/v4/timeline.js
@@ -2,13 +2,12 @@ function getTimeframe(startDate, endDate, gapsPerUnit, unit) {
 	let diff = -1;
 	if(luxon && timezone)
 		diff = luxon.DateTime.fromISO(endDate).setZone(timezone).diff(luxon.DateTime.fromISO(startDate), unit)[unit];
-	console.log(startDate, endDate, diff);
+	// console.log(startDate, endDate, diff);
 	return diff * gapsPerUnit;
 }
 
 function generateVerticalTimeline(id, data, width, height) {
 	let base = document.querySelector('#' + id);
-    base.style.width = '480px';
 	
 	let timeline = document.createElement('div');
 	timeline.classList.add('block');
@@ -59,70 +58,86 @@ function generateRow(parentElem, onClick, item) {
 	parentElem.appendChild(container);
 }
 
-function generateCell(parentElem, className, val) {
+function generateCells(parentElem, repeat, className, val) {
+	for(i = 0; i < parseInt(repeat); i++)
+	{
+		generateCell(parentElem, className, val);
+	}	
+}
+
+function generateCell(parentElem, className, val, onClick, onMouseover, onMouseout) {
 	let cell = document.createElement('div');
 	cell.classList.add('dimmed');
 	if(className) cell.classList.add(className);
 	if(val) cell.innerText = val;
+	if(onClick) cell.addEventListener('click', onClick);
+	if(onMouseover) cell.addEventListener('mouseover', onMouseover);
+	if(onMouseout) cell.addEventListener('mouseout', onMouseout);
 	if(parentElem) parentElem.appendChild(cell);
 }
 
 function generateHorizontalTimeline(id, data, width, height) {
 	let base = document.querySelector('#' + id);
-    base.style.width = '480px';
 	
 	let timeline = document.createElement('div');
 	timeline.classList.add('block');
 	timeline.classList.add('grid-horiz');
 	if(width) timeline.style.width = width;
 	if(height) timeline.style.height = height;
-	
+			
 	let container = document.createElement('div');
 	container.classList.add('container-horiz');
-
-	let date = document.createElement('div');
-	date.classList.add('thumb-horiz');
-	date.classList.add('dimmed');
-	container.appendChild(date);
 	
-	let blob = document.createElement('div');
-	blob.classList.add('blob-horiz');
-	blob.classList.add('dimmed');
-	container.appendChild(blob);
-	
-	let show = document.createElement('div');
-	show.classList.add('description-horiz');
-	show.classList.add('dimmed');
-	container.appendChild(show);
-	
-	timeline.appendChild(container);
-		
-	let counter = 1;
-			
+	let counter = 0;
+	let prev = null;
 	for(let item of data)
 	{
-		let blob = document.createElement('div');
-		blob.classList.add('blob-horiz');
-		blob.classList.add('dimmed');
-		blob.innerText = '-';
-		blob.setAttribute('data-id', counter++);
-		blob.addEventListener('click', function() {
-			let description = document.querySelectorAll('.grid-horiz .description-horiz')[parseInt(this.getAttribute('data-id'))];
-			description.click();
-		});
-		blob.addEventListener('mouseover', function() {
-			let thumb = document.querySelectorAll('.grid-horiz .thumb-horiz')[parseInt(this.getAttribute('data-id'))];
-			thumb.classList.add('selected');
-			let description = document.querySelectorAll('.grid-horiz .description-horiz')[parseInt(this.getAttribute('data-id'))];
-			description.classList.add('selected');
-		});
-		blob.addEventListener('mouseout', function() {
+		if(prev) {
+			let gaps = Math.floor(getTimeframe(prev, item.date, 1, 'months'));
+			if(gaps > 0) {
+				generateCells(container, gaps, 'thumb-horiz', '');
+			}			
+		}
+		
+		generateCell(container, 'thumb-horiz', item.date);
+		
+		prev = item.date;
+	}
+	
+	timeline.appendChild(container);
+	
+	container = document.createElement('div');
+	container.classList.add('container-horiz');
+
+	prev = null;
+	for(let item of data)
+	{
+		if(prev) {
+			let gaps = Math.floor(getTimeframe(prev, item.date, 1, 'months'));
+			if(gaps > 0) {
+				generateCells(container, gaps, 'blob-horiz', '-');
+			}			
+		}
+		
+		generateCell(container, 'blob-horiz', '-', 
+		function() {
+			let index = Array.from(this.parentElement.querySelectorAll('.blob-horiz')).indexOf(this);
+			generateProfileFromJSON(document.querySelectorAll('.grid-horiz .description-horiz')[index]);
+			
+		},
+		function() {
+			let index = Array.from(this.parentElement.querySelectorAll('.blob-horiz')).indexOf(this);
+			document.querySelectorAll('.grid-horiz .thumb-horiz')[index].classList.add('selected');
+			document.querySelectorAll('.grid-horiz .description-horiz')[index].classList.add('selected');
+		}, 
+		function() {
 			for(let select of document.querySelectorAll('.grid-horiz .selected'))
 			{
 				select.classList.remove('selected');
-			}
+			}			
 		});
-		container.appendChild(blob);
+		
+		prev = item.date;
 	}
 	
 	timeline.appendChild(container);
@@ -130,31 +145,19 @@ function generateHorizontalTimeline(id, data, width, height) {
 	container = document.createElement('div');
 	container.classList.add('container-horiz');
 
+	prev = null;
 	for(let item of data)
 	{
-		let date = document.createElement('div');
-		date.classList.add('thumb-horiz');
-		date.classList.add('dimmed');
-		date.innerText = item.date;
-			
-		container.appendChild(date);
-	}
-	
-	timeline.appendChild(container);
-	
-	container = document.createElement('div');
-	container.classList.add('container-horiz');
-	
-	for(let item of data)
-	{
-		let show = document.createElement('div');
-		show.classList.add('description-horiz');
-		show.classList.add('dimmed');
-		show.innerText = item.name;
-		show.addEventListener('click', function() {
-			generateProfileFromJSON(this);
-		});
-		container.appendChild(show);
+		if(prev) {
+			let gaps = Math.floor(getTimeframe(prev, item.date, 1, 'months'));
+			if(gaps > 0) {
+				generateCells(container, gaps, 'description-horiz', '');
+			}			
+		}
+		
+		generateCell(container, 'description-horiz', item.name);
+		
+		prev = item.date;
 	}
 	
 	timeline.appendChild(container);
