eb4b8b26a4513344c01c129d70df5ea709b697c3
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-03-11 21:38:34 +0800

    Update config.js

--- klassic-note-web/config.js
+++ klassic-note-web/config.js
@@ -2,8 +2,8 @@
 const debugMode = false;					// will show all available logging on console
 const defaultTitle = 'Klassic Note Web'; 	// title of browser and page, will be appended with song info when playing
 const databaseFilename = 'https://knneo.github.io/klassic-note-web/db/KlassicNote.db'; // location of database, url only
-const directory = 'file://C:/Users/KAINENG/OneDrive/Music/'; 			// location of audio file, in {KNYEAR}/{Filename}
-const coverArtDirectory = 'file:///F:/RBKN/Pictures/ART/ALBUMART/'; 	// location of cover art, in {KNYEAR}/{Filename}
+const directory = 'file://KAINENG-PC/songs/'; 			// location of audio file, in {KNYEAR}/{Filename}
+const coverArtDirectory = 'file://KAINENG-PC/albumart/'; 	// location of cover art, in {KNYEAR}/{Filename}
 const useDirectoryFormat = true; // for directory and coverArtDirectory, if false will not use format above
 const widescreenAverageModuleSize = 480; 	// on wide screens, (responsive) tab width for modules
 const autoplayOnSelect = false; 			// disable player autoplay on select song in any table
