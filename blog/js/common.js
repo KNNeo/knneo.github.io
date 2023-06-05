84701eee7ca396363a32744323d1d2da0fbc34a3
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-06-05 22:24:14 +0800

    fix apple music popup display (blogspot)

--- blog/js/common.js
+++ blog/js/common.js
@@ -340,9 +340,10 @@ function generatePopupContent(url) {
     }
     if (url.includes('music.apple.com')) {
         //process itunes embed
-        return '<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="'+ (!url.includes('i=') ? '450' : '150') +'" sandbox="allow-modals allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" src="' +
+		let isTrack = url.includes('i=');
+        return '<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="'+ (!isTrack ? 450 : 170) +'" sandbox="allow-modals allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" src="' +
             url.replace('music.apple.com', 'embed.music.apple.com') +
-            '" style="background: transparent; max-width: ' + (isSmallWidth() ? 290 : 660) + 'px; overflow: hidden; width: 100%;"></iframe>';
+            '" style="background: transparent; max-width: ' + (isSmallWidth() && !isTrack ? 290 : 660) + 'px; overflow: hidden; width: 100%;"></iframe>';
     }
     if (url.includes('twitter.com')) {
         //process twitter embed
