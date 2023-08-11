7cc932f36ae8246d8cf6928e490abd51e3111825
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-08-11 20:58:00 +0800

    Update blogger.js

--- blog/js/blogger.js
+++ blog/js/blogger.js
@@ -139,7 +139,7 @@ function reduceResults() {
 			let latestPostSummary = document.createElement('div');
 			latestPostSummary.classList.add('latest-post-summary');
 			let excerpt = snippet.innerText.trim();
-			latestPostSummary.innerHTML = (title == undefined ? '' : excerpt.substring(0, excerpt.substring(380, 450).indexOf(' ') + 380) + '...');
+			latestPostSummary.innerHTML = (title == undefined ? '' : excerpt.substring(0, excerpt.substring(300, 380).indexOf(' ') + 300) + '...');
 			
 			let innerWrapper = document.createElement('div');
 			if(link != undefined && thumb != undefined) innerWrapper.appendChild(latestPostThumb);
