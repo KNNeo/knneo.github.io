39bf184abc73248dec93e9bb7d0c163dc277c1c1
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-09-01 22:36:21 +0800

    optimize darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -2,49 +2,43 @@ const defaultLightModeTheme = window['light-theme'] ?? 'white';
 const defaultDarkModeTheme = window['dark-theme'] ?? 'black';
 const defaultDarkModeItem = window['dark-name'] ?? 'theme';
 
-setDarkMode();
-addDarkModeEvents();
-
 function setDarkMode() {
-	let theme = Array.from(document.getElementsByTagName('meta')).filter(m => m.name == 'theme-color');
+	let theme = document.querySelector('meta[name="theme-color"]');
 	let themeColor = document.createElement('meta');
 	if(theme && theme.length > 0)
-	{
-		themeColor = theme[0];
-	}
-	else
-	{
-		themeColor = document.createElement('meta');
+		themeColor = theme;
+	else {
 		themeColor.name = 'theme-color';
+		themeColor.content = defaultLightModeTheme;
 		document.head.appendChild(themeColor);
 	}
 	
+	//save as local storage
+	localStorage.setItem(defaultDarkModeItem, themeColor.content);
+	
+	//conditions to initialize dark mode
 	if(localStorage.getItem(defaultDarkModeItem) == defaultDarkModeTheme) toggleDarkMode();
 	else if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleDarkMode();
-	themeColor.content = document.getElementsByTagName('html')[0].classList.contains('darked') ? defaultDarkModeTheme : defaultLightModeTheme;
-	localStorage.setItem(defaultDarkModeItem, themeColor.content);
+	
+	window.addEventListener('load', addDarkModeEvents);
 }
 
 function addDarkModeEvents() {
-	//assume supports dark mode
+	//assume browser supports dark mode
 	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', toggleDarkMode);
-	//assume page has button with id = darkmode
-	if(document.querySelector('.darkmode') != null)
-		document.querySelector('.darkmode').addEventListener('click', toggleDarkMode);
+	//assume page has button with classname darkmode, add onclick event if not assigned
+	if(document.querySelector('.darkmode') && !document.querySelector('.darkmode').onclick)
+		document.querySelector('.darkmode').onclick = toggleDarkMode;
 }
 
 function toggleDarkMode() {
-	let theme = Array.from(document.getElementsByTagName('meta')).filter(m => m.name == 'theme-color');
-	let themeColor = theme[0];
-	if(document.getElementsByTagName('html')[0].classList.contains('darked')) //parent class of each page
-	{
-		document.getElementsByTagName('html')[0].classList.remove('darked');
-		themeColor.content = defaultLightModeTheme;
-	}
+	let theme = document.querySelector('meta[name="theme-color"]');
+	if(document.documentElement.classList.contains('darked')) //parent class of each page
+		theme.content = defaultLightModeTheme;
 	else
-	{
-		document.getElementsByTagName('html')[0].classList.add('darked');
-		themeColor.content = defaultDarkModeTheme;
-	}
-	localStorage.setItem(defaultDarkModeItem, themeColor.content);
+		theme.content = defaultDarkModeTheme;
+	document.documentElement.classList.toggle('darked');
+	localStorage.setItem(defaultDarkModeItem, theme.content);
 }
+
+setDarkMode();
