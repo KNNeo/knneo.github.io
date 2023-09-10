48442530364bd453fb4348614b4ce1666e22d7b4
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-09-10 17:22:35 +0800

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -5,34 +5,35 @@ const defaultDarkModeItem = window['dark-name'] ?? 'theme';
 function setDarkMode() {
 	let saved = localStorage.getItem(defaultDarkModeItem);
 	let theme = document.querySelector('meta[name="theme-color"]');
-	//conditions to initialize dark mode
+	// conditions to initialize dark mode
 	if(window.matchMedia('(prefers-color-scheme: dark)').matches) toggleDarkMode();
 	else {
 		let themeColor = document.createElement('meta');
-	if(theme && theme.length > 0) //document theme (assume no toggle on DOM, else always override)
-		themeColor = theme;
-	else { //no theme 
-		themeColor.name = 'theme-color';
-		themeColor.content = defaultLightModeTheme;
-		document.head.appendChild(themeColor);
-	}
+		if(theme && theme.length > 0) // document theme (assume no toggle on DOM, else always override)
+			themeColor = theme;
+		else { // no theme 
+			themeColor.name = 'theme-color';
+			themeColor.content = defaultLightModeTheme;
+			document.head.appendChild(themeColor);
+			theme = themeColor; // put back
+		}
 	
-	//save as local storage
-	localStorage.setItem(defaultDarkModeItem, themeColor.content);
+		//save as local storage
+		localStorage.setItem(defaultDarkModeItem, themeColor.content);
 	}
 
 	// override if have saved
 	if(saved && theme && saved != theme.content)
 		toggleDarkMode();
 
-	// if .darkmode button in DOM, to toggle modes
+	// if button .darkmode in DOM, to toggle modes
 	window.addEventListener('load', addDarkModeEvents);
 }
 
 function addDarkModeEvents() {
-	//assume browser supports dark mode
+	// assume browser supports dark mode
 	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', toggleDarkMode);
-	//assume page has button with classname darkmode, add onclick event if not assigned
+	// assume page has button with classname darkmode, add onclick event if not assigned
 	if(document.querySelector('.darkmode') && !document.querySelector('.darkmode').onclick)
 		document.querySelector('.darkmode').onclick = toggleDarkMode;
 }
