3a0675410ae704e17ae29290ac4ccf9d788176c9
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-10-23 22:29:21 +0800

    Update HallOfFame_NewArtists.sql

--- klassic-note-web/db/HallOfFame_NewArtists.sql
+++ klassic-note-web/db/HallOfFame_NewArtists.sql
@@ -1,2 +1,6 @@
 --List of New Artists
-select KNYEAR, group_concat(ArtistTitle, ' | ') from song s1 where ArtistTitle not in (select ArtistTitle from song s2 where s2.KNYEAR <= s1.KNYEAR - 1) group by KNYEAR
+--Run subquery for full list, full query for summary
+select a.KNYEAR, group_concat(a.ArtistTitle, ' | ') as 'New Artists'
+from (
+select distinct KNYEAR, ArtistTitle from song s1 where ArtistTitle not in (select ArtistTitle from song s2 where s2.KNYEAR <= s1.KNYEAR - 1)
+) a group by a.KNYEAR
