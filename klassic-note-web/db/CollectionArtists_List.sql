2251be0e8471fd4f7e924f4a9e90047c0c77964b
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-11-11 12:00:57 +0800

    Update CollectionArtists_List.sql

--- klassic-note-web/db/CollectionArtists_List.sql
+++ klassic-note-web/db/CollectionArtists_List.sql
@@ -2,10 +2,12 @@ select artisttitle, count(artisttitle) as Count, min(datecreated) as Started fro
 --need to include artist/collabartist table to check parentartist? song parentartist field not enough for collab songs
 
 select p.Parent as ArtistTitle, 
+SongList,
 sum(p.SongCount) as Count 
 from (
 select a.ArtistTitle, case when a.ArtistCode in ('ID') or s.ReleaseArtistTitle = 'V.A.' then a.ArtistTitle else a.ParentArtist end as Parent, 
 (select count(ParentArtist) from Artist aa where aa.ParentArtist = a.ParentArtist) as ParentArtistCount,
+group_concat(s.SongTitle, ' | ') as SongList,
 count(s.SongTitle) as SongCount from Song s
 join Artist a on s.ArtistID = a.ID
 where a.ArtistCode not in ('AS','AG') --exclude anime credit
