c349b84a308c142190de2f55304d3d9668458526
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-12-28 15:05:44 +0800

    SOTD scripts

--- klassic-note-web/db/Query_SOTDDay_TopSongsByYear.sql
+++ klassic-note-web/db/Query_SOTDDay_TopSongsByYear.sql
@@ -1,6 +1,6 @@
 select SongTitle, ArtistTitle, ArtistTitle || ' - ' || SongTitle as 'DisplayValue', count(SongTitle) as 'Count' from SOTD 
 where date between 20230104 and 20231227 --date range, inclusive
+and TimeOfDay = 'DAY' --songs by day/night
 and IsShortPreview = 0 --not preview flag
---and IsPastYear = 1 --mentioned more than once but not same year
 group by SongTitle, ArtistTitle having Count(SongTitle) > 0 --min count
 order by count(SongTitle) desc
