8269bea6b205aee1bfa62069c2673899ba6de62f
Neo Kai Neng <neo.kai.neng@gmail.com>
2023-12-30 19:36:33 +0800

    Update Analysis_SongAppetiteSurvey.sql

--- klassic-note-web/db/Analysis_SongAppetiteSurvey.sql
+++ klassic-note-web/db/Analysis_SongAppetiteSurvey.sql
@@ -1,23 +1,23 @@
-SELECT 'Jan' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.01.%'
+SELECT 'Jan' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.01.%'
 UNION ALL
-SELECT 'Feb' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.02.%'
+SELECT 'Feb' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.02.%'
 UNION ALL
-SELECT 'Mar' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.03.%'
+SELECT 'Mar' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.03.%'
 UNION ALL
-SELECT 'Apr' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.04.%'
+SELECT 'Apr' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.04.%'
 UNION ALL
-SELECT 'May' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.05.%'
+SELECT 'May' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.05.%'
 UNION ALL
-SELECT 'Jun' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.06.%'
+SELECT 'Jun' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.06.%'
 UNION ALL
-SELECT 'Jul' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.07.%'
+SELECT 'Jul' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.07.%'
 UNION ALL
-SELECT 'Aug' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.08.%'
+SELECT 'Aug' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.08.%'
 UNION ALL
-SELECT 'Sep' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.09.%'
+SELECT 'Sep' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.09.%'
 UNION ALL
-SELECT 'Oct' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.10.%'
+SELECT 'Oct' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.10.%'
 UNION ALL
-SELECT 'Nov' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.11.%'
+SELECT 'Nov' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.11.%'
 UNION ALL
-SELECT 'Dec' AS 'Month', COUNT(SongID) AS 'Count' FROM Song WHERE KNYEAR = 2021 AND DateCreated LIKE '2021.12.%'
+SELECT 'Dec' AS 'Month', COUNT(ID) AS 'Count' FROM Song WHERE KNYEAR = 2023 AND DateCreated LIKE (KNYEAR || '%') AND DateCreated LIKE '%.12.%'
