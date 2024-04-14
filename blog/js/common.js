8ae6cbfac233f5eff70562944125bde429b64ee9
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-14 12:54:24 +0800

    prevent hashchange on popstate valid (blogspot)

--- blog/js/common.js
+++ blog/js/common.js
@@ -53,18 +53,21 @@ function windowOnHistoryChange() {
 	// if viewer open, close and return
 	if(document.querySelector('.viewer')?.classList.contains('open')) {
 		event.preventDefault();
+		window['history-change'] = true;
 		closeViewer();
 		return;
 	}
 	// if dialog open, close and return
 	if(document.querySelector('.dialog') != null) {
 		event.preventDefault();
+		window['history-change'] = true;
 		removeDialog();
 		return;
 	}
 	// if popup open, close and return
 	if(document.querySelector('.overlay') != null) {
 		event.preventDefault();
+		window['history-change'] = true;
 		closePopups();
 		return;
 	}
@@ -171,6 +174,11 @@ function addHashtags() {
 }
 
 function scrollToSectionByUrl() {
+	if(window['history-change']) { // if triggered popstate, revert hashchange
+		event.preventDefault();
+		history.forward();
+		window['history-change'] = false;
+	}
 	if(window.location.hash.length > 0)
 		scrollToElement(document.getElementById(window.location.hash.substring(1)));
 }
