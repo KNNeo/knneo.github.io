6e56cf9e7afad482892d3006f19a3bd99b3f8a11
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-26 22:11:53 +0800

    redo sw by fetch (blogspot)

--- blog/js/sw.js
+++ blog/js/sw.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = 'klassic-note-web-reports-v1';
+const REQUEST_DESTINATIONS = ['image', 'script', 'style', 'font'];
 
 self.addEventListener('install', function(event) {
   // Perform install steps
@@ -6,53 +7,32 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(getCacheUrls());
+        return cache;
       })
   );
 });
 
-function getCacheUrls() {
-	let list = ['/'];	
-	// all header assets
-	list.concat(Array.from(document.querySelectorAll('link,script')).map(l => l.href));	
-	// all images
-	list.concat(Array.from(document.querySelectorAll('img')).map(i => i.src));	
-	return list;
-}
-
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-	caches.match(event.request)
-	  .then(function(response) {
-		// Cache hit - return response
-		if (response) {
-		  return response;
-		}
-
-		// Clone the request
-		var fetchRequest = event.request.clone();
-
-		return fetch(fetchRequest).then(
-		  function(response) {
-			// Check if we received a valid response
-			if(!response ||
-			   response.status !== 200 ||
-			   response.type !== 'basic'
-			) {
-			  return response;
-			}
-
-			// Clone the response
-			var responseToCache = response.clone();
-
-			caches.open(CACHE_NAME)
-			  .then(function(cache) {
-				cache.put(event.request, responseToCache);
-			  });
-
-			return response;
-		  }
-		);
-	  })
-  );
+  // Check if the request is for an image
+  if (REQUEST_DESTINATIONS.includes(event.request.destination)) {
+    const request = event.request;
+    event.respondWith(
+      caches.match(request) // Try to serve from cache first
+        .then((cachedResponse) => {
+          // If found in cache, return it
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+          // If not cached, fetch from network and cache the response
+          return fetch(request)
+            .then((response) => {
+              // Clone the response for caching
+              const responseClone = response.clone();
+              caches.open(CACHE_NAME)
+                .then((cache) => cache.put(request, responseClone));
+              return response;
+            });
+        })
+    );
+  }
 });
