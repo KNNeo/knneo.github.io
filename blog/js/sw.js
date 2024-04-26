e4c7b4a6de61ad6c825b7557bf7a5003e281fc1e
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-26 22:14:23 +0800

    Update sw.js

--- blog/js/sw.js
+++ blog/js/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache;
+        return cache.addAll(['/']);
       })
   );
 });
