6b9a208262e17f354227ecff59d93ff8a0bf0a6b
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-26 22:55:51 +0800

    Update sw.js

--- blog/sw.js
+++ blog/sw.js
@@ -13,22 +13,29 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     const request = event.request;
-    event.respondWith(
-      caches.match(request) // Try to serve from cache first
-        .then((cachedResponse) => {
-          // If found in cache, return it
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-          // If not cached, fetch from network and cache the response
-          return fetch(request)
-            .then((response) => {
-              // Clone the response for caching
-              const responseClone = response.clone();
-              caches.open(CACHE_NAME)
-                .then((cache) => cache.put(request, responseClone));
-              return response;
-            });
-        })
-    );
-});
+	// console.log('Fetch event triggered for:', request.url); // Log the requested URL
+	// Check if online (navigator.onLine is a boolean indicating online/offline state)
+	if (navigator.onLine) {
+		// Fetch from network if online
+		event.respondWith(fetch(request));
+	} else {
+		event.respondWith(
+		  caches.match(request) // Try to serve from cache first
+			.then((cachedResponse) => {
+			  // If found in cache, return it
+			  if (cachedResponse) {
+				return cachedResponse;
+			  }
+			  // If not cached, fetch from network and cache the response
+			  return fetch(request)
+				.then((response) => {
+				  // Clone the response for caching
+				  const responseClone = response.clone();
+				  caches.open(CACHE_NAME)
+					.then((cache) => cache.put(request, responseClone));
+				  return response;
+				});
+			})
+		);
+	}
+);
