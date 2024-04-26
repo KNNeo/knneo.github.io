fe2c299138a082e2965d91d57299cc0aee5fd2f7
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-26 22:40:32 +0800

    Update index.js

--- blog/index.js
+++ blog/index.js
@@ -11,7 +11,7 @@ window.addEventListener('hashchange', filterByTag);
 // service worker implementation
 function addServiceWorker() {
 	if (navigator && 'serviceWorker' in navigator) {
-	  navigator.serviceWorker.register('js/sw.js')
+	  navigator.serviceWorker.register('js/sw.js', { scope: '/' })
 		.then(function(registration) {
 			console.log('Service worker registered:', registration.scope);
 		}, function(err) {
