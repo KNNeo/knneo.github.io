43ef6f81480a8a9d5d1d4fc9001acef89205462b
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-30 20:12:30 +0800

    cleanup sw (blogspot)

--- blog/sw.js
+++ blog/sw.js
@@ -16,17 +16,8 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  // Perform fetch of any url in application
   const request = event.request;
-  const url = new URL(request.url);
-  const fileType = url.pathname.split('.').pop();
-  // Decide cache based on Accept header
-  let cacheName = CACHE_NAME_STATIC;
-  if (request.headers.get('Accept').includes('text/html'))
-	cacheName = CACHE_NAME_PAGES;
-  else if (request.headers.get('Accept').includes('text/css') || 
-  request.headers.get('Accept').includes('application.javascript'))
-	cacheName = CACHE_NAME_RESOURCES;
-  
   event.respondWith(
     caches.match(request) 
       .then((cachedResponse) => {
@@ -36,11 +27,19 @@ self.addEventListener('fetch', function(event) {
 		}
         // Check online status
         if (navigator.onLine) {
+		  // Decide cache based on Accept header
+		  let cacheName = CACHE_NAME_STATIC;
+		  if (request.headers.get('Accept').includes('text/html'))
+			cacheName = CACHE_NAME_PAGES;
+		  else if (request.headers.get('Accept').includes('text/css') || 
+		  request.headers.get('Accept').includes('application/javascript'))
+			cacheName = CACHE_NAME_RESOURCES;
           // Online, fetch from network and potentially cache for future use
           return fetch(request)
             .then((response) => {
               // Clone the response for caching
-              const responseClone = response.clone();
+              const responseClone = response.clone();		
+			  // Open cache and add response
               caches.open(cacheName)
                 .then((cache) => cache.put(request, responseClone));
               return response;
@@ -57,6 +56,7 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('activate', (event) => {
+  // Perform delete of cache if name does not exist
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -71,6 +71,7 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('sync', function(event) {
+  // Perform update of static images (subject to browser implementation)
   if (event.tag === 'update-cache') {
     event.waitUntil(
       caches.open(CACHE_NAME_STATIC)
@@ -78,9 +79,10 @@ self.addEventListener('sync', function(event) {
           return cache.keys().then((keys) => {
             return Promise.all(
               keys.map((key) => {
-                // Check if the key represents an image resource (e.g., using URL or extension)
+                // Check if the key represents an image resource
                 if (isImageResource(key)) {
-                  return fetch(key) // Fetch the image from the network
+				  // If is image, fetch from network
+                  return fetch(key) 
                     .then((response) => {
                       if (response.ok) {
                         // Update cache with the new response
@@ -98,6 +100,6 @@ self.addEventListener('sync', function(event) {
 });
 
 function isImageResource(key) {
-  // as long as it's static image, won't catch loaded image files ie. must end with extension
+  // As long as it's static image, won't catch loaded image files ie. must end with extension
   return /\.(png|jpg|jpeg|gif)$/.test(key.url);
 }
