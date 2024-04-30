113557124cfaccd4bde6b9ff1e529ee50be77d4b
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-04-30 20:19:20 +0800

    Update sw.js

--- blog/sw.js
+++ blog/sw.js
@@ -31,7 +31,7 @@ self.addEventListener('fetch', function(event) {
 		  let cacheName = CACHE_NAME_STATIC;
 		  if (request.headers.get('Accept').includes('text/html'))
 			cacheName = CACHE_NAME_PAGES;
-		  else if (request.headers.get('Accept').includes('text/css') || 
+		  if (request.headers.get('Accept').includes('text/css') || 
 		  request.headers.get('Accept').includes('application/javascript'))
 			cacheName = CACHE_NAME_RESOURCES;
           // Online, fetch from network and potentially cache for future use
