49f6191e3c4b44d8d35fc1b5629999b78490c76f
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-05-25 17:14:47 +0800

    fix firefox masonry render error (site)

--- blog/pages/js/grid.js
+++ blog/pages/js/grid.js
@@ -373,6 +373,8 @@ function renderMasonry(sectionNo, index, component) {
 	
 	let gallery = document.createElement('div');
 	gallery.classList.add(component.type);
+	// fix firefox percentage fail on td child display flex
+	gallery.style.height = elem.getBoundingClientRect().height + 'px';
 	
 	let totalCol = Math.min(Math.floor(window.innerWidth / 200), 5); // smallest denominator for largest screens
 	// sort items before render
@@ -433,30 +435,32 @@ function shiftMasonryItems() {
 	// shift items up if possible
 	let gallery = event.target.closest('.masonry');
 	// if fully loaded, then continue
-	if(Array.from(gallery.querySelectorAll('img')).filter(i => i.complete).length != gallery.querySelectorAll('img').length)
+	if(gallery && Array.from(gallery.querySelectorAll('img')).filter(i => i.complete).length != gallery.querySelectorAll('img').length)
 		return;
 	// find all column heights
-	let columns = gallery.querySelectorAll('.column');
-	let columnHeights = Array.from(columns).map(g => g.getBoundingClientRect().height);
-	// console.log(columnHeights);
-	// if have disparity
-	let minHeight = Math.min(...columnHeights);
-	let maxHeight = Math.max(...columnHeights);
-	if(maxHeight - minHeight > diff) {
-		// destination: shortest height column
-		let destColumn = columns[columnHeights.indexOf(minHeight)];
-		// console.log(destColumn);
-		// source: column with last item equal or lesser than disparity
-		let sourceColumn = Array.from(columns).filter(c => c.querySelectorAll('img')[c.querySelectorAll('img').length-1].clientHeight <= diff && c != destColumn);
-		if(sourceColumn.length > 0) {
-			sourceColumn = sourceColumn[0];
-			// console.log(sourceColumn);
-			// move image from source to dest column
-			let source = sourceColumn.querySelectorAll('img')[sourceColumn.querySelectorAll('img').length-1];
-			let clone = source.cloneNode(true);
-			clone.addEventListener('click', onClickGalleryImage);
-			source.remove();
-			destColumn.appendChild(clone);
+	let columns = gallery?.querySelectorAll('.column');
+	if(columns) {
+		let columnHeights = Array.from(columns).map(g => g.getBoundingClientRect().height);
+		// console.log(columnHeights);
+		// if have disparity
+		let minHeight = Math.min(...columnHeights);
+		let maxHeight = Math.max(...columnHeights);
+		if(maxHeight - minHeight > diff) {
+			// destination: shortest height column
+			let destColumn = columns[columnHeights.indexOf(minHeight)];
+			// console.log(destColumn);
+			// source: column with last item equal or lesser than disparity
+			let sourceColumn = Array.from(columns).filter(c => c.querySelectorAll('img')[c.querySelectorAll('img').length-1].clientHeight <= diff && c != destColumn);
+			if(sourceColumn.length > 0) {
+				sourceColumn = sourceColumn[0];
+				// console.log(sourceColumn);
+				// move image from source to dest column
+				let source = sourceColumn.querySelectorAll('img')[sourceColumn.querySelectorAll('img').length-1];
+				let clone = source.cloneNode(true);
+				clone.addEventListener('click', onClickGalleryImage);
+				source.remove();
+				destColumn.appendChild(clone);
+			}
 		}
 	}
 }
