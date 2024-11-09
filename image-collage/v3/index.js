b8c6f17072bad2a2bb30b98e4404c5d6bb376a95
Neo Kai Neng <neo.kai.neng@gmail.com>
2024-11-09 16:19:20 +0800

    move config into data js (image-collage-v3)

--- image-collage/v3/index.js
+++ image-collage/v3/index.js
@@ -1,64 +1,4 @@
-//--NOTES--//
-//Default separator is '_' (underscore) unless stated in settings
-//Filenames of images to be in the format of <tag>_<tag>, if with extension eg. '.jpg' will be removed
-//Thumbnail filenames to be in format '<prefix>_<id>' according to preset; original has no prefix
-//Data in data.js, variable mosaicArray
-
-//--SETTINGS--//
-const config = {
-	debug: false,
-	menu: {
-		width: 500,
-	},
-	title: 'VVブロマイド図鑑',
-	description: `Gallery based on tag separated filenames\n\n©コーエーテクモゲームス All rights reserved.	`,
-	placeholder: {
-		include: '以内の…',
-		exclude: '以外の…',
-	},
-	sort: {
-		order: 'asc',
-		locale: 'ja-JP',
-		property: 'nm',
-	},
-	setting: {
-		sidebar: true,
-		clear: true,
-		expand: true,
-		darkmode: true,
-		preset: true,
-		filter: true,
-		slideshow: 5,
-		stats: true,
-	},
-	tag: {
-		category: {
-			groups: ['衣装', 'キャラクター'],
-			ratio: [4, 1],
-		},
-		preset: [160, 320, 640],
-		min: 2,
-		max: 9999,
-		exclude: ['覚醒'],
-		hidden: ['2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023'],
-	},
-	grid: {
-		banner: {
-			length: 1,
-		},
-		column: {
-			min: 2,
-		},
-		thumbnail: {
-			ratio: 9/16,
-		},
-		star: {
-			text: '⭐',
-			tooltip: '覚醒'
-		}
-	},
-	separator: '_',
-};
+//--CONSTANTS--//
 const isHorizontalLayout = function() { 
 	return matchMedia('all and (orientation:landscape)').matches; 
 }
@@ -295,7 +235,7 @@ function generateViewer() {
 }
 
 function generateStats() {
-	let filtered = generateFiltered(mosaicArray)
+	let filtered = generateFiltered(config.data)
 	.reduce(function(total, current, arr) {
 		let names = getFilenameInfo(current.nm).filename.split(config.separator);
 		for(let name of names) {
@@ -402,12 +342,12 @@ function generateGrid() {
 }
 
 function generateFiltered() {
-	if(typeof mosaicArray != 'object') return [];
+	if(typeof config.data != 'object') return [];
 	let includeArray = window['includeCriteria'].split('|');
 	let excludeArray = window['excludeCriteria'].split('|');
 	if(config.debug) console.log('included', includeArray);
 	if(config.debug) console.log('excluded', excludeArray);
-	return mosaicArray.filter(m => 
+	return config.data.filter(m => 
 		(window['includeCriteria'].length == 0 || includeArray.filter(s => m.nm.toLowerCase().includes(s.toLowerCase() + config.separator) || m.nm.toLowerCase().includes(config.separator + s.toLowerCase())).length == includeArray.length) && 
 		(window['excludeCriteria'].length == 0 || excludeArray.filter(s => !m.nm.toLowerCase().includes(s.toLowerCase() + config.separator) && !m.nm.toLowerCase().includes(config.separator + s.toLowerCase())).length == excludeArray.length) &&
 		(config.tag.exclude ?? []).filter(f => m.nm.includes(f)).length < 1
