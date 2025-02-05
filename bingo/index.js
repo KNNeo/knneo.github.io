2b60995815bff481ef195cca20a4622b83f06629
Neo Kai Neng <neo.kai.neng@gmail.com>
2025-02-05 12:06:32 +0800

    fix toggle daub not rendering title (bingo)

--- bingo/index.js
+++ bingo/index.js
@@ -215,8 +215,7 @@ function toggleDaub() {
 	let id = config.daubs.indexOf(window['daub']);
 	window['daub'] = (id + 1 >= config.daubs.length) ? config.daubs[0] : config.daubs[id + 1];
 	localStorage.setItem('daub', window['daub']);
-	let colors = window['daub'].split('-');
-	document.documentElement.style.setProperty('--daub', document.documentElement.classList.contains('darked') && colors.length > 1 ? colors[1] : colors[0]);
+	renderTitle();
 }
 
 function toggleCards() {
