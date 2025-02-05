15b887556aa66e81ebaf3ccb84e0cb2f8f97c0bc
Neo Kai Neng <neo.kai.neng@gmail.com>
2025-02-05 15:06:58 +0800

    basic drag and drop (categorizer)

--- categorizer/index.js
+++ categorizer/index.js
@@ -29,11 +29,33 @@ let pageSection = document.querySelector('section.page');
 let drawerSection = document.querySelector('section.drawer');
 
 //--DOM FUNCTIONS--//
-
-
-//--EVENT HANDLERS--//
 function onDismissHeader() {
     header.classList.add('hidden');
+}
+
+//--EVENT HANDLERS--//
+function dragItem() {
+    let elem = event.target;
+    let id = '.item[data-id="' + elem.dataset.id + '"][data=category="' + elem.dataset.category + '"]';
+    event.dataTransfer.setData("text", id);
+}
+
+function allowDrop() {
+    event.preventDefault();
+}
+
+function dropItem() {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.querySelector(data));
+}
+
+function toggleDrawer() {
+    drawerSection.classList.toggle('hidden');
+}
+
+function selectPreset() {
+
 }
 
 //--FUNCTIONS--//
@@ -46,5 +68,54 @@ function getUniqueCategories(list) {
 
 //--INITIAL--//
 function startup() {
-    window.data = config.preset[0];
+    window.data = config.presets[0];
+    window.data.categories = getUniqueCategories(window.data.list);
+    renderCanvas();
+    renderCategories();
+    renderItems();
+    renderDrawer();
+}
+
+function renderCanvas() {
+    let canvasDiv = document.createElement('div');
+    canvasDiv.className = 'canvas';
+    canvasDiv.id = window.data.id;
+    pageSection.appendChild(canvasDiv);
+}
+
+function renderCategories() {
+    let canvas = document.querySelector('#' + window.data.id);
+    for(let category of window.data.categories) {
+        let categoryDiv = document.createElement('div');
+        categoryDiv.className = 'category';
+        categoryDiv.setAttribute('data-id', category);
+        categoryDiv.setAttribute('ondragover', 'allowDrop()');
+        categoryDiv.setAttribute('ondrop', 'dropItem()');
+        canvas.appendChild(categoryArea);
+    }
+}
+
+function renderItems() {
+    for(let item of window.data.list) {
+        let itemDiv = document.createElement('div');
+        itemDiv.className = 'item';
+        itemDiv.setAttribute('draggable', 'true');
+        itemDiv.setAttribute('ondragstart', 'dragItem()');
+        itemDiv.setAttribute('data-id', item.id);
+        if(item.category) itemDiv.setAttribute('data-category', item.category);
+        itemDiv.innerText = item.name;
+
+        let canvas = document.querySelector('.category[data-id="' + item.category + '"]');
+        if(canvas) // put in category
+            canvas.appendChild(itemDiv);
+        else // put in 
+            drawerSection.appendChild(itemDiv);
+    }
+}
+
+function renderDrawer() {
+    let closeButton = document.createElement('button');
+    button.setAttribute('onclick', 'toggleDrawer()');
+
+    drawerSection.appendChild(closeButton);
 }
