7e1dc646c6b8d901e7ff77f40e07c42ed0d956aa
Neo Kai Neng <neo.kai.neng@gmail.com>
2025-07-05 23:12:04 +0800

    add system default, contextmenu toggle

--- darkmode.js
+++ darkmode.js
@@ -1,8 +1,10 @@
 const defaultLightTheme = window['light-theme'] ?? 'white';
 const defaultDarkTheme = window['dark-theme'] ?? 'black';
+const systemDefaultTheme = window['system-theme'] ?? 'default';
 const defaultThemeItem = window['dark-name'] ?? 'theme';
+const prefersColorScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-function setDarkMode() { 
+function setInitialTheme() { 
 	let saved = localStorage.getItem(defaultThemeItem);
 	let theme = document.querySelector('meta[name="theme-color"]');
 	// conditions to initialize dark mode
@@ -13,8 +15,10 @@ function setDarkMode() {
 			themeColor.content = saved;
 			document.head.appendChild(themeColor);
 		}
-		if(saved == defaultDarkTheme) // if theme is dark, toggle
+		if(saved == defaultDarkTheme) // saved theme is dark
 			toggleDarkMode();
+        if(saved == systemDefaultTheme && prefersColorScheme.matches) // system default
+            toggleDarkMode('auto');
 	}
 	else { // no save data
 		if (!theme) { // no meta on DOM
@@ -24,12 +28,20 @@ function setDarkMode() {
 			document.head.appendChild(themeColor);
 			localStorage.setItem(defaultThemeItem, themeColor.content); // save data
 		}
-		if (window.matchMedia('(prefers-color-scheme: dark)').matches) { // if device dark mode
+		if (prefersColorScheme.matches) { // if device dark mode
 			toggleDarkMode(); // toggle, will save data
 		}
 	}
 	// if button .darkmode in DOM, allow toggle modes
 	window.addEventListener('load', addDarkModeEvents);
+	// auto mode setup
+	prefersColorScheme.addEventListener('change', function(event) {
+		if(localStorage.getItem(defaultThemeItem) == systemDefaultTheme) {
+            if((!event.matches && document.documentElement.classList.contains('darked'))
+                || (event.matches && !document.documentElement.classList.contains('darked')))
+                toggleDarkMode('auto'); // toggle, will save data
+        }
+	});
 }
 
 function addDarkModeEvents() {
@@ -38,14 +50,28 @@ function addDarkModeEvents() {
 		document.querySelector('.darkmode').onclick = toggleDarkMode;
 }
 
-function toggleDarkMode() {
+function toggleDarkMode(mode) {
 	let theme = document.querySelector('meta[name="theme-color"]');
 	if(document.documentElement.classList.contains('darked')) //parent class of each page
 		theme.content = defaultLightTheme;
 	else
 		theme.content = defaultDarkTheme;
 	document.documentElement.classList.toggle('darked');
-	localStorage.setItem(defaultThemeItem, theme.content);
+    // if not triggered by change event, update storage
+    if(typeof mode != 'string' || mode != 'auto')
+        localStorage.setItem(defaultThemeItem, theme.content);
 }
 
-setDarkMode();
+function customSetTheme() {
+    // for setting system default
+    event.preventDefault();
+    let all = [defaultLightTheme,defaultDarkTheme,systemDefaultTheme];
+    let content = '';
+    while (content != null && !all.includes(content)) {
+        content = prompt('Key in theme value\n' + all.join('|'), systemDefaultTheme);
+    }
+    localStorage.setItem(defaultThemeItem, content);
+    prefersColorScheme.dispatchEvent(new Event('change'));
+}
+
+setInitialTheme();
