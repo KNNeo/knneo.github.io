f358849582ebcfe8efd757d1976a480584e3a1f5
Neo Kai Neng <neo.kai.neng@gmail.com>
2025-07-20 18:16:05 +0800

    update anime data season 2025 summer

--- shows-list/v2/index.js
+++ shows-list/v2/index.js
@@ -1,6 +1,6 @@
 //--SETTINGS--//
 const currentYear = 2025;
-const currentSeason = 'Spring';
+const currentSeason = 'Summer';
 const maxPerSeason = 7;
 const seasons = [
 	{ title:'Winter', altTitle:'' },
