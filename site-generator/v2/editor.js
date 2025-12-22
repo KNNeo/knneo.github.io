82d99e00165362c7e1281b14e37ef73a258aa9c5
Neo Kai Neng <neo.kai.neng@gmail.com>
2025-12-22 15:15:05 +0800

    add edit masonry item context menu, check on json input (site-generator-v2)

--- site-generator/v2/editor.js
+++ site-generator/v2/editor.js
@@ -12,8 +12,85 @@ function onMasonryContextMenu() {
 			let sectionIndex = parseInt(section.getAttribute('data-index'));
 			let gridItem = document.context.closest('.grid-item');
 			let gridItemIndex = parseInt(gridItem.style.getPropertyValue('--idx'));
-            //obtain input (key in for now)
+            //obtain user input
 			window.input = {};
+			let jsonInput = prompt('key in json (optional)');
+			if(jsonInput) window.input = JSON.parse(jsonInput);
+			if(!window.input.title)
+				window.input.title = prompt('key in title', window.input.title);
+			if(!window.input.title) return;
+			if(!window.input.artist)
+				window.input.artist = prompt('key in artist', window.input.artist);
+			if(!window.input.artist) return;
+			if(!window.input.thumbnail)
+				window.input.thumbnail = prompt('key in image url', window.input.thumbnail);
+			if(!window.input.thumbnail) return;
+			if(!window.input.title_url)
+				window.input.title_url = prompt('key in title url in format: title|url', window.input.title_url || 'Melonbooks|');
+			if(!window.input.title_url) return;
+			if(!window.input.artist_url)
+				window.input.artist_url = prompt('key in artist url in format: artist|url', window.input.artist_url || 'Artist Twitter/X|');
+			if(!window.input.artist_url) return;
+			if(!window.input.translation)
+				window.input.translation = prompt('key in translation of title (optional)', window.input.translation || '');
+			if(!window.input.tags)
+				window.input.tags = prompt('key in tags, pipeline [|] separated (optional)', window.input.tags || '');
+			if(window.input.tags)
+				window.input.tags = window.input.tags.split('|');
+            //fixed template
+			let template = { 
+				"tooltip": window.input.title, 
+				"thumbnail": window.input.thumbnail, 
+				"grid": { 
+					"type": "grid", "columns": 2, "rows": 8, 
+					"items": [
+						{ "type": "image", "rows": 8, "tooltip": "", "source": window.input.thumbnail },
+						{ "type": "title", "rows": 6, "prefix": window.input.translation, "title": window.input.title, "suffix": window.input.artist, "links": [
+							{ "text": window.input.title_url.split('|')[0], "url": window.input.title_url.split('|')[1] },
+							{ "text": window.input.artist_url.split('|')[0], "url": window.input.artist_url.split('|')[1] }
+						]},
+						{ "type": "tags", "rows": 2, "prefix": "#", "filter": true, "values": window.input.tags || [] }
+					]
+				},
+				"width": 450,
+				"height": 637
+			};
+            //add into data object and render again
+			config.data.pages[sectionIndex].items[gridItemIndex - 1].images.push(template);
+			save();
+			render();
+		}},
+		{ title: 'Add Masonry Item (Melonbooks)', disabled: true, onclick: function() {
+			window.input = {};
+			window.input.source_url = prompt('key in url');
+			if(!window.input.source_url) return;
+			getText(window.input.source_url, addMasonryItemMelonbooks);
+		}}
+	];
+	if(event.target.closest('img')) {
+		options.push({ title: 'Edit Masonry Item', onclick: function() {
+			//find position of section and section item in data object
+			let section = document.context.closest('section');
+			let sectionIndex = parseInt(section.getAttribute('data-index'));
+			let gridItem = document.context.closest('.grid-item');
+			let gridItemIndex = parseInt(gridItem.style.getPropertyValue('--idx'));
+			let image = document.context;
+			let imageIndex = parseInt(image.getAttribute('data-images'));
+            //obtain data objects
+			let data = config.data.pages[sectionIndex].items[gridItemIndex - 1].images[imageIndex];
+			let gridImage = data.grid.items[0];
+			let gridTitle = data.grid.items[1];
+			let gridTags = data.grid.items[2];
+			window.input = {
+				title: gridTitle.title,
+				artist: gridTitle.suffix,
+				thumbnail: gridImage.source,
+				title_url: gridTitle.links[0].url,
+				artist_url: gridTitle.links[1].url,
+				translation: gridTitle.prefix,
+				tags: gridTags.values.join('|')
+			};
+            //obtain user input
 			let jsonInput = prompt('key in json (optional)');
 			if(jsonInput) window.input = JSON.parse(jsonInput);
 			window.input.title = prompt('key in title', window.input.title);
@@ -48,19 +125,11 @@ function onMasonryContextMenu() {
 				"width": 450,
 				"height": 637
 			};
-            //add into data object and render again
-			config.data.pages[sectionIndex].items[gridItemIndex - 1].images.push(template);
+            //load from data object and render again
+			config.data.pages[sectionIndex].items[gridItemIndex - 1].images[imageIndex] = template;
 			save();
 			render();
-		}},
-		{ title: 'Add Masonry Item (Melonbooks)', disabled: true, onclick: function() {
-			window.input = {};
-			window.input.source_url = prompt('key in url');
-			if(!window.input.source_url) return;
-			getText(window.input.source_url, addMasonryItemMelonbooks);
-		}}
-	];
-	if(event.target.closest('img')) {
+		}});
 		options.push({ title: 'Remove Masonry Item', onclick: function() {
 			//find position of section and section item in data object
 			let section = document.context.closest('section');
