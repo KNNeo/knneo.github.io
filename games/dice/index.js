6ba68a3a310846dc559fcbda40d3787efcb193cb
Neo Kai Neng <neo.kai.neng@gmail.com>
2026-02-05 11:06:10 +0800

    fix variable name (dice)

--- games/dice/index.js
+++ games/dice/index.js
@@ -122,7 +122,7 @@ function end() { // from trigger, end game
 
 	// record layout
 	let opponentCells = Array.from(document.querySelectorAll('.opponent.cell')).map(c => parseInt(c.getAttribute('data-id') || '0'));
-	let playerCels = Array.from(document.querySelectorAll('.player.cell')).map(c => parseInt(c.getAttribute('data-id') || '0'));
+	let playerCells = Array.from(document.querySelectorAll('.player.cell')).map(c => parseInt(c.getAttribute('data-id') || '0'));
 
 	scores.unshift({ player: playerScore, opponent: opponentScore, win: whoWins, layout: { opponent: opponentCells, player: playerCells } });
 
