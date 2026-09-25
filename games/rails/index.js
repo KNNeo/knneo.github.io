cc87c412989b9b22691192c5bbfeea59d1217155
Neo Kai Neng <neo.kai.neng@gmail.com>
2026-09-25 12:13:32 +0800

    fix

--- games/rails/index.js
+++ games/rails/index.js
@@ -608,6 +608,8 @@ function renderMissions(list) {
 }
 
 function onMissionActionClick() {
+	// prevent multi-click
+	event.target.disabled = true;
 	// add to missions list
 	if (!window.data.game.mission.list)
 		window.data.game.mission.list = [];
